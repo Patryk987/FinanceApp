45c3c8beade8eaf350843605efbefea7d6e261ef
Adrian Binda <adrian.binda@gmail.com>
2023-04-01 11:40:59 +0200

    poprawiony con

--- FinanceApp/Entities/FinanceAppContext.cs
+++ FinanceApp/Entities/FinanceAppContext.cs
@@ -37,7 +37,7 @@ public virtual DbSet<Users> Users { get; set; }
 
 protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-    => optionsBuilder.UseSqlServer("Data Source=ADRIAN\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;");
+    => optionsBuilder.UseSqlServer("Data Source=ADRIAN\\SQLEXPRESS;Initial Catalog=FinanceApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
 protected override void OnModelCreating(ModelBuilder modelBuilder)
 {
